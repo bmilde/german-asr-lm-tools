b97c6df81361b4e9bc0e8f1711e7345fc8013d1d
Benjamin Milde <milde@ltgpu2.informatik.uni-hamburg.de>
2020-06-09 12:36:53 +0200

    fixed Wikipedia steps, added and normalized europarl

--- convert_wiki_norm.sh
+++ convert_wiki_norm.sh
@@ -5,7 +5,7 @@ for i in {0..9}; do
    python3 normalize_wiki_sentences.py wikiextractor_output/AA/wiki_0$i.bz2 norm/0$i.txt &
 done
 
-for i in {10..46}; do
+for i in {10..90}; do
    echo "process $i"
    python3 normalize_wiki_sentences.py wikiextractor_output/AA/wiki_$i.bz2 norm/$i.txt &
 done
