6ce1dfa0b8000355bfc58977a690a20a3550bb46
bmilde <bmilde@M>
2022-04-24 23:36:34 +0200

    Added most common repeating trigrams

--- statistics.py
+++ statistics.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 import argparse
 voc = defaultdict(int)
+common_repeating_ngrams = defaultdict(int)
 linenum = 0
 
 parser = argparse.ArgumentParser()
@@ -9,6 +10,12 @@ parser.add_argument('-o', '--output', required=True)
 args = parser.parse_args()
 files = args.files[0]
 output_voc = args.output
+
+def ngrams(input_list, n):
+# from https://stackoverflow.com/questions/17531684/n-grams-in-python-four-five-six-grams
+    if n<=1:
+        return [[elem] for elem in input_list]
+    return zip(*[input_list[i:] for i in range(n)])
 
 # num_range = 45
 # files = ["train_text.txt"]
@@ -26,17 +33,26 @@ for filename in files:
         for line in in_file:
             if line[-1] == '/n':
                 line = line[:-1]
+            split = line.split()
             for word in line.split():
                 voc[word] += 1
+            for trigram in ngrams(split, 4):
+                if trigram[0] == trigram[1] and trigram[1] == trigram[2] and trigram[2]==trigram[3]:
+                    common_repeating_ngrams[' '.join(trigram)] += 1
+
             linenum += 1
             if linenum % 10000 == 0:
                 print("file",filename,"linenum:",linenum,line)
 
 sorted_voc = sorted(voc.items(), key=lambda kv: kv[1], reverse=True)
 
-print("First hundered words:")
+sorted_rep_trigrams = sorted(common_repeating_ngrams.items(), key=lambda kv: kv[1], reverse=True)
 
+print("First hundered words:")
 print(sorted_voc[:100])
+
+print("Most common repeating trigrams:")
+print(sorted_rep_trigrams[:300])
 
 with open(output_voc,"w") as out_file:
     for tup in sorted_voc:
