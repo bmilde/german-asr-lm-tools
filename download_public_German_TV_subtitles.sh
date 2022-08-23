b2556b92738d8680538d8d973e0afc055ddbc305
Alienmaster <robert.geislinger@uni-hamburg.de>
2022-08-23 16:21:51 +0200

    bigger range for subtitles

--- download_public_German_TV_subtitles.sh
+++ download_public_German_TV_subtitles.sh
@@ -3,7 +3,7 @@
 mkdir mediathek_subs/
 cd mediathek_subs/
 
-for i in {53001..600000}
+for i in {1..600000}
 do
 	echo "trying $i"
 	echo "wget -nc --max-redirect 0 https://classic.ardmediathek.de/subtitle/$i"
